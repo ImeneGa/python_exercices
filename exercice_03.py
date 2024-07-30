f366ecc48344cf774466d49c7af82eec9f7b48d7
Imy_Ga <imyy.ga@gmail.com>
2024-07-30 18:00:11 +0200

    Janus number

---
+++ exercice_03.py
@@ -0,0 +1,40 @@
+def isInt(n):
+    try:
+        int(n)
+        return True
+    except ValueError:
+        return False
+    
+def isJanus(n):
+    j = 0
+    a = 0
+    for i in(n):
+        a = a + int(i) * (10 ** j)
+        j = j + 1
+    return a == int(n)
+
+val1 = True
+val2 = True
+tab = []
+
+print("You're looking for Janus numbers between a and b")
+while val1:
+    a = input('Enter the value of a\n')
+    if isInt(a):
+        val1 = False
+        while val2:
+            b = input('Enter the value of b\n')
+            if isInt(b):
+                val2 = False
+                if (int(a) > int(b)):
+                    print('b must be more than a! Try again')
+                    val2 = True
+                else:
+                    for i in range(int(a), int(b)+1):
+                        if isJanus(str(i)):
+                            tab.append(i)
+                    print("Janus numbers between", a,"and", b,"are:\n", tab)
+            else:
+                print(b, 'is not an integer! Try again')         
+    else:
+        print(a, 'is not an integer! Try again')                            
